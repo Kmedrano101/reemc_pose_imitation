1f9ccbe0307c5f66f9c70b38d32c07f380eeabb6
kmedrano101 <kevin.ejem18@gmail.com>
2022-08-20 21:00:59 +0200

    Web UI and Transformations v1

--- scripts/human_pose.py
+++ scripts/human_pose.py
@@ -3,6 +3,7 @@
 # Importar modulos
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
 from cv_bridge import CvBridge, CvBridgeError
 from rospy.exceptions import ROSException
 from geometry_msgs.msg import Point
@@ -13,13 +14,24 @@ from os import system
 import cv2 as cv
 from copy import deepcopy
 import numpy as np
-
 # Parametros y variables
+try:
+    rospy.get_param_names()
+except ROSException:
+    print("[WARNING] No se puede obtener los nombres de parametros")
 
 PACKAGE_NAME = "/reemc_pose_imitation"
 NODE_NAME = "human_pose_points"
-TOPIC_S1_CAMERA = "/usb_cam/image_raw"
+IMG_MODE = rospy.get_param("image_source/mode", default=0)
+TOPIC_S1_IMG = '/usb_cam/image_raw'
+TOPIC_S2_IMG = '/activar_movimientos'
+if int(IMG_MODE) == 1:
+    TOPIC_S1_IMG = rospy.get_param("image_source/video_path",default="/videofile/image")
+if int(IMG_MODE) == 2:
+    TOPIC_S1_IMG = rospy.get_param("image_source/usb_cam",default="/usb_cam/image_raw")
 TOPIC_P1_POSE = "pose_human"
+IMG_PATH = '/home/kmedrano101/catkin_ws/src/reemc_pose_imitation/img/' # Cambiar dir otrher machine user name change
+IMG_NAME = rospy.get_param("image_source/name", default='javi1')
 BRIDGE = CvBridge()
 
 # Clase HumanPose
@@ -29,8 +41,8 @@ class HumanPose:
     """HumanPose object"""
 
     def __init__(self):
-        self._subTopicFlagName = None
         self._subTopicImageSourceName = None
+        self._subTopicActionName = None
         self._pubTopicStatusNodeName = None
         self._pubTopicPoseNodeName = None
         self.cvFrame = []
@@ -40,6 +52,9 @@ class HumanPose:
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose()
         self.readyCapturePose = False
+        self.imgName = ''
+        self.lastImage = []
+        self.statusAction = False
 
     """Properties"""
 
@@ -56,6 +71,17 @@ class HumanPose:
             rospy.loginfo("Invalid Name of topic ImageSource")
 
     @property
+    def subTopicActionName(self):
+        """The subTopicActionName property."""
+        return self._subTopicActionName
+
+    @subTopicActionName.setter
+    def subTopicActionName(self, value):
+        if value:
+            self._subTopicActionName = value
+        else:
+            rospy.loginfo("Invalid Name of topic ImageSource")
+    @property
     def pubTopicPoseNodeName(self):
         """The pubTopicDataNodeName property."""
         return self._pubTopicPoseNodeName
@@ -69,19 +95,22 @@ class HumanPose:
 
     """ Methods and Actions"""
 
-    def find_pose(self, draw=True) -> None:
-        imgRGB = cv.cvtColor(self.cvFrame, cv.COLOR_BGR2RGB)
+    def find_pose(self,img, draw=True) -> None:
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
         if self.results.pose_landmarks:
             if draw:
                 self.mpDraw.draw_landmarks(
                     self.cvFrame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
 
-    def find_position(self, draw=True) -> None:
+    def find_position(self,img, draw=True) -> None:
         lmAux = human_pose()
         dataPoint = Point()
         dataPose = point_pose()
         contador = 0
+        # Puntos de pose para validar, en caso de faltar un punto en la imagen
+        # no se procedera a realizar las funciones de movimiento
+        pointsValidate = [0,11,12,13,14,15,16]
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 dataPoint.x = lm.x
@@ -90,21 +119,27 @@ class HumanPose:
                 dataPose.id_pos = id
                 dataPose.position = dataPoint
                 dataPose.visibility = lm.visibility
-                if id == 0 or id == 15 or id == 16 or id == 11 or id == 12:
+                if id in pointsValidate:
                     if lm.visibility > 0.5:
                         contador += 1
                 lmAux.points_pose.append(deepcopy(dataPose))
-            if contador >= 3:
+            if contador >= 7:
                 self.readyCapturePose = True
             else:
                 self.readyCapturePose = False
-        if len(self.cvFrame) > 0:
+        if len(img) > 0:
             cv.namedWindow("IMAGE_FROM_HUMAN_POSE", cv.WINDOW_NORMAL)
             cv.moveWindow("IMAGE_FROM_HUMAN_POSE", 1280, 0)
-            cv.imshow("IMAGE_FROM_HUMAN_POSE", self.cvFrame)
+            cv.imshow("IMAGE_FROM_HUMAN_POSE", img)
             cv.resizeWindow("IMAGE_FROM_HUMAN_POSE", 640, 480)
             cv.waitKey(1)
         self.pubTopicDataNode.publish(lmAux)
+        if(self.statusAction):
+                cv.imwrite(IMG_PATH+'img_poses.jpg',self.cvFrame)
+                self.statusAction = False
+                rospy.loginfo("Save img!")
+        self.set_param_values()
+        #print("shape of img",img.shape)
 
     def image_source_callback(self, msg) -> None:
         self.dataReceivedTopic1 = True
@@ -113,18 +148,27 @@ class HumanPose:
         except CvBridgeError as e:
             print(f"[ERROR] {e}")
 
-    def flag_callback(self, msg) -> None:
-        self.dataReceivedTopic2 = True
-        self.statusFlag = msg
+    def action_callback(self, msg) -> None:
+        self.statusAction = msg.data
+        rospy.loginfo(self.statusAction)
 
     def start_subscribers(self) -> None:
-        rospy.Subscriber(self._subTopicImageSourceName,
-                        Image, self.image_source_callback)
+        rospy.Subscriber(self.subTopicActionName,Bool,self.action_callback)
+        if int(IMG_MODE) > 0:
+            rospy.Subscriber(self.subTopicImageSourceName,Image, self.image_source_callback)
 
     def start_publishers(self) -> None:
         self.pubTopicDataNode = rospy.Publisher(
             self._pubTopicPoseNodeName, human_pose, queue_size=10)
 
+    def get_param_values(self) -> None:
+        # Obtener todos los paramatros 
+        self.imgName = rospy.get_param("image_source/name", default='javi1')
+
+    def set_param_values(self) -> None:
+        h,w,_ = self.cvFrame.shape
+        rospy.set_param("/image_source/weight",w)
+        rospy.set_param("/image_source/height",h)
 
 def main():
     system('clear')
@@ -136,19 +180,29 @@ def main():
     rospy.loginfo(f"NODO {NODE_NAME} INICIADO.")
     """Inicializar el objeto HumanPose"""
     objNode = HumanPose()
-    objNode.subTopicImageSourceName = TOPIC_S1_CAMERA
+    objNode.subTopicImageSourceName = TOPIC_S1_IMG
+    objNode.subTopicActionName = TOPIC_S2_IMG
     objNode.pubTopicPoseNodeName = TOPIC_P1_POSE
     objNode.start_subscribers()
     objNode.start_publishers()
     INFO1 = True
     INFO2 = True
+    # Modo de recuso de imagen video-stream o imagen
+    if int(IMG_MODE) == 0:
+        objNode.dataReceivedTopic1 = True
+    
+    ### MAKE FUNCTION FOR VALIDATIONS ALL DATA NEEDED
     while not rospy.is_shutdown():
+        objNode.get_param_values()
+        if int(IMG_MODE) == 0:
+                path_img = IMG_PATH+objNode.imgName
+                objNode.cvFrame = cv.imread(path_img)
         if not objNode.dataReceivedTopic1 and INFO2:
-            print("[WARNING] Datos no recibidos")
+            print("[WARNING] Datos no recibidos, esperando datos...")
             INFO2 = False
         elif np.array(objNode.cvFrame).size:
-            objNode.find_pose()
-            objNode.find_position(draw=False)
+            objNode.find_pose(objNode.cvFrame)
+            objNode.find_position(objNode.cvFrame,draw=False)
             if objNode.readyCapturePose and INFO1:
                 rospy.loginfo("READY TO CAPTURE DATA")
                 INFO1 = False
--- scripts/transformations_pose.py
+++ scripts/transformations_pose.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
 # Importar modulos
+from ntpath import join
 import rospy
 from rospy.exceptions import ROSException
 from reemc_pose_imitation.msg import human_pose
 import time
 import os
 import numpy as np
-import json
+import math
 # Parametros y variables
+
+try:
+    rospy.get_param_names()
+except ROSException:
+    print("[WARNING] No se puede obtener los nombres de parametros")
 
 PACKAGE_NAME = "/reemc_pose_imitation"
 NODE_NAME = "transformations_pose"
-TOPIC_S1_POSE = PACKAGE_NAME+"/pose_human"
-
+TOPIC_S1_POSE = "/pose_human"
 # Class Agent
 
 
@@ -24,9 +29,13 @@ class Transformations:
         self._subTopicHumanPoseName = None
         self.dataHumanPose = human_pose()
         self.paramPoints = []
+        self.localPoints = []
         self.paramJointNames = []
         self.points = []
         self.webPoints = []
+        self.posePoints = []
+        self.imgWeight = 0
+        self.imgHeight = 0
 
     """ Properties """
     @property
@@ -54,32 +63,114 @@ class Transformations:
         pass
 
     def get_param_values(self) -> None:
-        # Params from play_motion file
-        self.paramJointNames = rospy.get_param('/play_motion/motions/my_move/joints')
-        self.paramPoints = rospy.get_param('/play_motion/motions/my_move/points')
+        # Obtener todos los paramatros de los archivos .yaml dentro de reem-c y reemc_pose_imitation
+        self.paramJointNames = rospy.get_param('/play_motion/motions/my_move2/joints')
+        self.paramPoints = rospy.get_param('/play_motion/motions/my_move2/points')
+        self.imgWeight = rospy.get_param("/image_source/weight")
+        self.imgHeight = rospy.get_param("/image_source/height")
         Points = list(self.paramPoints[0].values())
         self.points = Points[0]
+        self.localPoints = rospy.get_param('/local_points/points')
         # Params from web UI interface
         self.webPoints = rospy.get_param('/web_points/points')
+        self.mode = rospy.get_param("/operation_mode", default=0)
 
     def set_param_values(self) -> None:
         AuxparamPoints = self.paramPoints[0]
         AuxparamPoints['positions'] = self.points
         self.paramPoints[0] = AuxparamPoints
-        rospy.set_param('/play_motion/motions/my_move/points',self.paramPoints)
+        rospy.set_param('/play_motion/motions/my_move2/points',self.paramPoints)
         # Intentar actualizar solo Joints de interes 
 
+    def get_angle(self, p1, p2, p3) -> float: # input values make it as a dict 
+        angle = 0
+        points = []
+        if self.dataHumanPose.points_pose:
+            for pose in self.dataHumanPose.points_pose:
+                h, w = self.imgHeight, self.imgWeight # height of img and weight of img
+                cx, cy = int(pose.position.x * w), int(pose.position.y * h)
+                points.append([pose.id_pos, cx, cy]) # pose.position.z
+            x1, y1 = points[p1][1:]
+            x2, y2 = points[p2][1:]
+            x3, y3 = points[p3][1:]
+            # Calculate the Angle
+            angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                                math.atan2(y1 - y2, x1 - x2))
+            if angle < 0:
+                angle += 360
+            self.posePoints = points
+        #print(angle)
+        return angle 
+
+    def map_value(self,value,out_min,out_max,in_min,in_max) -> float: # input map values min max make it in a dict of Joints [joint_1: [0.0,2.0,0.0,120.0]]
+        #value = float((value - robot_min) * (angle_max - angle_min) / (robot_max - robot_min) + angle_min)
+        value = float((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+        #print(value)
+        return value
+
+    def get_distance(self,x1,x2,y1,y2):
+        distance = np.sqrt((x2-x1)**2+(y2-y1)**2)
+        return distance
+
     def transformations(self) -> None:
-        # Order of Points
-        JointsList = [7,26,22,9,5,6,0,27,13,18,21,4,2,17,8,3,15,10]
         # Points to be modified: 
         self.get_param_values()
-        for i in range(len(JointsList)):
-            self.points[JointsList[i]] = self.webPoints[i]
-        self.set_param_values() 
-        print(self.paramJointNames)
-        time.sleep(1)
-        print("Modified: ", self.paramPoints)
+        # Obtener los angulos para los Joints del Robot
+        if self.mode == 0:
+            if self.get_angle(23,11,13):
+                # Joints Arm Left
+                angle_alfa1 = self.get_angle(13,11,23)
+                angle_beta1 = self.get_angle(15,11,23)
+                angle_gama1 = self.get_angle(15,13,11)
+                self.localPoints[self.paramJointNames.index('arm_left_2_joint')] = round(self.map_value(angle_alfa1,0.0,2.0,0,120),2)
+                if angle_beta1 > angle_alfa1:
+                    self.localPoints[self.paramJointNames.index('arm_left_3_joint')] = 1.5
+                else:
+                    self.localPoints[self.paramJointNames.index('arm_left_3_joint')] = -1.5
+
+                self.localPoints[self.paramJointNames.index('arm_left_4_joint')] = round(self.map_value(abs(185-angle_gama1),0.0,2.2,0.0,160.0),2)
+                # Joints Arm Right
+                angle_alfa2 = self.get_angle(24, 12, 14)
+                angle_beta2 = self.get_angle(24, 12, 16)
+                angle_gama2 = self.get_angle(12, 14, 16)
+                self.localPoints[self.paramJointNames.index('arm_right_2_joint')] = round(self.map_value(angle_alfa2,0.0,2.0,0,120),2)
+                if angle_beta2 > angle_alfa2:
+                    self.localPoints[self.paramJointNames.index('arm_right_3_joint')] = 1.5
+                else:
+                    self.localPoints[self.paramJointNames.index('arm_right_3_joint')] = -1.5
+
+                self.localPoints[self.paramJointNames.index('arm_right_4_joint')] = round(self.map_value(abs(185-angle_gama2),0.0,2.2,0.0,160.0),2) 
+                # Joints Head 
+                d1 = self.get_distance(self.posePoints[0][1],self.posePoints[7][1],self.posePoints[0][2],self.posePoints[7][2])
+                d2 = self.get_distance(self.posePoints[0][1],self.posePoints[8][1],self.posePoints[0][2],self.posePoints[8][2])
+                print("distancia 1: ",d1)
+                print("distancia 2: ",d2)
+                if d1 > d2:
+                    # Validar valor > Limites minimos
+                    if abs(d1-d2) < d1*0.58:
+                        value1 = d1*0.58
+                    else:
+                        value1 = abs(d1-d2)
+                    self.localPoints[self.paramJointNames.index('head_1_joint')] = -round(self.map_value(value1,0.0,1.6,0,(d1*0.58)),2)
+                else:
+                    # Validar valor > Limites minimos
+                    if abs(d2-d1) < d2*0.58:
+                        value2 = d2*0.58
+                    else:
+                        value2 = abs(d2-d1)
+                    self.localPoints[self.paramJointNames.index('head_1_joint')] = round(self.map_value(value2,0.0,1.6,0,(d2*0.58)),2)
+                for i in range(18):
+                    self.points[i] = self.localPoints[i]
+                self.set_param_values() 
+        else:
+            for i in range(18):
+                self.points[i] = self.webPoints[i]
+            self.set_param_values()
+            #print(self.paramJointNames)
+            #print(self.posePoints)
+            #print("2do Angulo: ",self.findAngle(13, 11, 23))
+            time.sleep(1)
+        #print("Modified: ", self.paramPoints)
         
 
 def main():
@@ -98,7 +189,6 @@ def main():
     while not rospy.is_shutdown():
         objNode.transformations()
     rospy.spin()
-
 
 if __name__ == '__main__':
     try:
